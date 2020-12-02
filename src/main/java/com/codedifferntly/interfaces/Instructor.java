c4ce221f69d2634bd61f60f058822a0fcf4533d1
victor840 <victor.ugoanusi@gmail.com>
2020-12-01 21:56:04 -0500

    Teacher Interface, Instructor Class & Test

---
+++ src/main/java/com/codedifferntly/interfaces/Instructor.java
@@ -0,0 +1,21 @@
+package com.codedifferntly.interfaces;
+
+import com.codedifferntly.interfaces.interfaces.Learner;
+import com.codedifferntly.interfaces.interfaces.Teacher;
+
+public class Instructor extends Person implements Teacher {
+    public Instructor(long id, String name){
+        super(id, name);
+    }
+
+    @Override
+    public void teach(Learner learner, double numberOfHours) {
+    learner.Learn(numberOfHours);
+    }
+
+    @Override
+    public void lecture(Learner[] learners, double numberOfHours) {
+
+    }
+
+}
