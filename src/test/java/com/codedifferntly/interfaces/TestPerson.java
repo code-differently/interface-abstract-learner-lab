698bb4ebb6faba5d0d91b8c8434cd7562f457433
victor840 <victor.ugoanusi@gmail.com>
2020-12-01 19:23:40 -0500

    made Person test and Perosn class

--- src/test/java/com/codedifferntly/interfaces/TestPerson.java
+++ src/test/java/com/codedifferntly/interfaces/TestPerson.java
@@ -1,5 +1,49 @@
 package com.codedifferntly.interfaces;
 
-public class TestPerson {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestPerson {
+    @Test
+    public void testConstructor(){
+        //Given
+        Person person  = new Person(007l,"Vic");
+
+        //When
+        long expected = 007l;
+        long actual = person.getID();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void constructorNameTest(){
+        //Given
+        Person person = new Person(007l, "Vic");
+
+        //When
+        String expected = "Vic";
+        String actual = person.getName();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setNameTest(){
+        //Given
+        Person person = new Person(007l,"Vic");
+        person.setName("Sam");
+
+        //When
+        String expected = "Sam";
+        String actual = person.getName();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+
+    }
 }
