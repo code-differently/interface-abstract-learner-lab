01e06e17993bb5799643769a7bcfa6a880d529b9
stainlessray <raymondcoolmail@gmail.com>
2020-12-01 21:32:01 -0500

    fixed mistake in formatting for Teacher Class

--- src/main/java/com/codedifferntly/interfaces/Teacher.java
+++ src/main/java/com/codedifferntly/interfaces/Teacher.java
@@ -1,12 +1,8 @@
 package com.codedifferntly.interfaces;
 
 public interface Teacher {
-<<<<<<< HEAD
+
     void teach(Learner learner, double numberOfHours);
 
     void lecture(Learner[] learners);
-=======
-     void teach(Learner learner, double numberOfHours);
-     void lecture(Learner[] learners);
->>>>>>> origin/ray
 }
