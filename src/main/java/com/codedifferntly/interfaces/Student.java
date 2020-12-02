6d15dd049340af48fe3e2972d22f4023330aebaf
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-01 19:49:02 -0500

    Create Learner interface, Student class, and some of the test

---
+++ src/main/java/com/codedifferntly/interfaces/Student.java
@@ -0,0 +1,28 @@
+package com.codedifferntly.interfaces;
+
+//Create a Student class such that:
+//        Student is a subclass of Person
+//        Student implements the Learner interface
+//Student should have an instance variable totalStudyTime of type double
+//        Student should have a concrete implementation of the learn method which increments the totalStudyTime variable by the specified numberOfHours argument.
+//        Student should have a getTotalStudyTime() method which returns the totalStudyTime instance variable.
+
+import com.codedifferntly.interfaces.interfaces.Learner;
+
+public class Student extends Person implements Learner {
+
+    private double totalStudyTime;
+
+    public Student(long id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void Learn(double numberOfHours) {
+        // increment totalStudyTime by numberOfHours
+    }
+
+    public double getTotalStudyTime() {
+        return 0.0;
+    }
+}
---
+++ src/main/java/com/codedifferntly/interfaces/interfaces/Learner.java
@@ -0,0 +1,12 @@
+package com.codedifferntly.interfaces.interfaces;
+
+//Create a Learner interface.
+//        Learner should declare one method signature:
+//        Method name: learn
+//        Method parameters: double numberOfHours
+//        Method return-type: void
+
+
+public interface Learner {
+    public void Learn(double numberOfHours);
+}
