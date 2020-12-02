b8ac717ee737ac876ddfe4d660315e9d89266954
Holly Chaffee <saltdc95@gmail.com>
2020-12-02 10:35:22 -0500

    Working on TestStudent

--- src/test/java/com/codedifferntly/interfaces/TestStudent.java
+++ src/test/java/com/codedifferntly/interfaces/TestStudent.java
@@ -1,4 +1,61 @@
+package com.codedifferntly.interfaces;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class TestStudent {
-  
+    //    Create a TestStudent class.
+    //    Create a testImplementation method that asserts that a Student is an instanceof a Learner.
+    //    Create a testInheritance method that asserts that a Student is an instanceof a Person.
+    //    Create a testLearn method that ensures a Student's totalStudyTime instance variable is
+            // incremented by the specified numberOfHours by invoking the .learn method.
+
+//    @Before
+//    public void setUp()   {  //throws Exception?
+//        Student testStudent = new Student(1);
+ //   }
+
+    @Test
+    public void testImplementation() {
+        //Given
+        Student testStudent = new Student(1);
+
+        //When
+        boolean isStudentInstanceOf = testStudent instanceof Learner;
+
+        //Then
+        Assert.assertTrue(isStudentInstanceOf);
+
+    }
+
+    @Test
+    public void testInheritance() {
+        //Given
+        Student testStudent = new Student(1);
+
+        //When
+        boolean isPersonInstanceOf = testStudent instanceof Person;
+
+        //Then
+        Assert.assertTrue(isPersonInstanceOf);
+
+    }
+
+    @Test
+    public void testLearn() {
+        //Given
+        Student testStudent = new Student(1);
+        testStudent.learn(5.0);
+        //When
+
+
+        //Then
+
+
+
+    }
 }
+
