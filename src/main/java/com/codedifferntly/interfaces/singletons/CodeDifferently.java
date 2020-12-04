51373a0a591e3c0e9433dc3e7f5a3b3c524ecf91
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-03 21:13:40 -0500

    Create CodeDifferently.java

---
+++ src/main/java/com/codedifferntly/interfaces/singletons/CodeDifferently.java
@@ -0,0 +1,35 @@
+package com.codedifferntly.interfaces.singletons;
+
+//    Create a CodeDifferently singleton.
+//        The class should declare a field that references the instance of Students called students.
+//        The class should declare a field that references the instance of Instructors called instructors.
+//        The class should define a method hostLecture which makes use of a Teacher teacher, double numberOfHours parameter to host a lecture to the composite people field in the students reference.
+//        The class should define a method hostLecture which makes use of a long id, double numberOfHours parameter to identify a respective Instructor to host a lecture to the composite people field in the cohort reference.
+
+import com.codedifferntly.interfaces.Instructor;
+import com.codedifferntly.interfaces.Student;
+import com.codedifferntly.interfaces.interfaces.Teacher;
+
+public final class CodeDifferently {
+
+    private static CodeDifferently INSTANCE = new CodeDifferently();
+    private Students students = Students.getInstance();
+    private Instructors instructors = Instructors.getInstance();
+
+    private CodeDifferently() {
+
+    }
+
+    public static CodeDifferently getInstance() {
+        return null;
+    }
+
+    public void hostLecture(Teacher teacher, double numberOfHours) {
+
+    }
+
+    public void hostLecture(long id, double numberOfHours) {
+
+    }
+
+}
